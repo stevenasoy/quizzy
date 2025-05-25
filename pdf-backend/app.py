027ae79b74bf4863d0f6bf1c62974b132f6c9505
stevenjakeasoy <stevenjakeasoy@gmail.com>
2025-05-25 11:17:17 +0800

    added port fix

--- pdf-backend/app.py
+++ pdf-backend/app.py
@@ -105,4 +105,5 @@ def extract_file():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
