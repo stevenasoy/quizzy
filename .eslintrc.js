d7db6794e55bb7bd237cf2f6432c3a428e330e68
stevenjakeasoy <stevenjakeasoy@gmail.com>
2025-05-26 22:23:25 +0800

    fixed after a lot of error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
